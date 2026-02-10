01d76438d9bda301dc91fadb7822a616dfc06672
Mister Milian <biloramez@gmail.com>
2026-02-10 15:26:29 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 buildscript {
-repositories {
-google()
-mavenCentral()
-}
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 plugins {
-id("com.android.application") version "8.2.2" apply false
-id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
