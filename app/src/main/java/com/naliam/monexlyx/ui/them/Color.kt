727267a82ba8ca1a2bb9e0c9b25ac9bbd5927d6b
Mister Milian <biloramez@gmail.com>
2026-02-10 15:44:59 +0200

    Update Color.kt

--- app/src/main/java/com/naliam/monexlyx/ui/them/Color.kt
+++ app/src/main/java/com/naliam/monexlyx/ui/them/Color.kt
@@ -3,52 +3,39 @@ package com.naliam.monexlyx.ui.theme
 import androidx.compose.ui.graphics.Color
 
 /* =======================
-   ألوان الوضع الفاتح
+   Light Theme Colors
    ======================= */
 
-val PrimaryLight = Color(0xFF2E7D32)          // أخضر مالي
+val PrimaryLight = Color(0xFF2E7D32)        // أخضر مالي
 val OnPrimaryLight = Color(0xFFFFFFFF)
 
-val PrimaryContainerLight = Color(0xFFC8E6C9)
-val OnPrimaryContainerLight = Color(0xFF1B5E20)
-
-val SecondaryLight = Color(0xFF1565C0)        // أزرق هادئ
+val SecondaryLight = Color(0xFF1565C0)      // أزرق ثقة
 val OnSecondaryLight = Color(0xFFFFFFFF)
 
-val SecondaryContainerLight = Color(0xFFDCE8FF)
-val OnSecondaryContainerLight = Color(0xFF0D47A1)
-
-val BackgroundLight = Color(0xFFF8FAF9)
-val OnBackgroundLight = Color(0xFF1C1C1C)
+val BackgroundLight = Color(0xFFF7F9FC)     // خلفية فاتحة مريحة
+val OnBackgroundLight = Color(0xFF1C1B1F)
 
 val SurfaceLight = Color(0xFFFFFFFF)
-val OnSurfaceLight = Color(0xFF1C1C1C)
+val OnSurfaceLight = Color(0xFF1C1B1F)
 
 val ErrorLight = Color(0xFFB3261E)
 val OnErrorLight = Color(0xFFFFFFFF)
 
-
 /* =======================
-   ألوان الوضع الداكن
+   Dark Theme Colors
    ======================= */
 
 val PrimaryDark = Color(0xFF81C784)
 val OnPrimaryDark = Color(0xFF003910)
 
-val PrimaryContainerDark = Color(0xFF1B5E20)
-val OnPrimaryContainerDark = Color(0xFFC8E6C9)
-
 val SecondaryDark = Color(0xFF90CAF9)
-val OnSecondaryDark = Color(0xFF00325A)
-
-val SecondaryContainerDark = Color(0xFF0D47A1)
-val OnSecondaryContainerDark = Color(0xFFDCE8FF)
+val OnSecondaryDark = Color(0xFF003258)
 
 val BackgroundDark = Color(0xFF121212)
-val OnBackgroundDark = Color(0xFFE6E6E6)
+val OnBackgroundDark = Color(0xFFE6E1E5)
 
 val SurfaceDark = Color(0xFF1E1E1E)
-val OnSurfaceDark = Color(0xFFE6E6E6)
+val OnSurfaceDark = Color(0xFFE6E1E5)
 
 val ErrorDark = Color(0xFFF2B8B5)
 val OnErrorDark = Color(0xFF601410)
